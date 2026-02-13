0f9d4655fdfd1b610b360dcc57095e5891019b9b
Varun Kumar <kumarvarun9811@gmail.com>
2026-02-13 13:12:13 +0530

    fixed build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import { GoogleTagManager } from "@next/third-parties/google";
 import ScrollToTop from "./components/helper/scroll-to-top";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import ToastProvider from "./components/ToastProvider";
 import { Inter } from "next/font/google";
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
@@ -12,9 +11,9 @@ import "./css/card.scss";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio of Abu Said - Software Developer",
+  title: "Portfolio of Varun Kumar - Software Developer",
   description:
-    "This is the portfolio of Abu Said. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+    "This is the portfolio of Varun Kumar. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
 };
 
 export default function RootLayout({
@@ -25,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ToastContainer />
+        <ToastProvider />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
           <Navbar />
           {children}
