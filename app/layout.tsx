4b04ac1db0ab336683ad04bc6dd6f02b163a0ce3
Varun Kumar <kumarvarun9811@gmail.com>
2026-02-13 13:14:25 +0530

    fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,7 @@ import { Inter } from "next/font/google";
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
 import { ReactNode } from 'react';
-import "./css/globals.scss";
-import "./css/card.scss";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
